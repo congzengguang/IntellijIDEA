45dacd4226c8ed62b11eae9bf34a04af52af8123
cong <1783366507@qq.com>
2015-06-07 21:53:17 +0800

    Spring demo

---
+++ Demo/src/cn/oracle/dbc/DatabaseConnection.java
@@ -0,0 +1,10 @@
+package cn.oracle.dbc;
+
+/**
+ * Created by Administrator on 2015/6/7.
+ */
+public class DatabaseConnection {
+    private static final String DBDRIVER="org.gjt.mm.mysql.Driver";
+    private static final String DBURL="jdbc:mysql://localhost:3306/oracle";
+
+}
