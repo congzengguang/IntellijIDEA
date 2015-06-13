d7dde86f2b04c1dacc8a0e1b3292a8be3257d451
cong <1783366507@qq.com>
2015-06-13 21:37:05 +0800

    Demo

---
+++ Demo/src/cn/oracle/test/DatabaseConnectionTest.java
@@ -0,0 +1,12 @@
+package cn.oracle.test;
+
+import cn.oracle.dbc.DatabaseConnection;
+
+/**
+ * Created by Administrator on 2015/6/13.
+ */
+public class DatabaseConnectionTest {
+    public static void main(String args[]){
+        System.out.println(new DatabaseConnection());
+    }
+}
